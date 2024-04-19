38d962a7b33c91f052e3e8edaa40221cea53f904
QuodEstDubitandum <d.steblin.dev@gmail.com>
2024-04-19 23:34:18 +0200

    autopair

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,30 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = true,
+    -- use opts = {} for passing setup options
+    -- this is equalent to setup({}) function
+  },
+  {
+    'numToStr/Comment.nvim',
+    opts = {
+      -- add any options here
+    },
+    lazy = false,
+  },
+
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    opts = {},
+    config = function(_, opts)
+      require('toggleterm').setup {
+        open_mapping = [[<C-j>]],
+      }
+    end,
+  },
+}
